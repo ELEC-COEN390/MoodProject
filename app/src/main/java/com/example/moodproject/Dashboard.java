2e3d766a13ba47326a4424572b2f15bf90a58328
JonaBaron <133152403+JonaBaron@users.noreply.github.com>
2025-03-25 21:36:34 -0400

    UI changes dashboard

--- app/src/main/java/com/example/moodproject/Dashboard.java
+++ app/src/main/java/com/example/moodproject/Dashboard.java
@@ -439,6 +439,16 @@ public class Dashboard extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         );
+
+        // Additional flags that help with full immersive mode
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
     }
 
     private void setupVideoBackground() {
